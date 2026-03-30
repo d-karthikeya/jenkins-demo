11fac2c5b5b58b1c416d21e21076bf61e23a764d
d-karthikeya <dara.karthikeya2023@vitstudent.ac.in>
2026-03-30 16:16:26 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Test{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter your Registration Number: ");
+        String regno = sc.nextLine();
+        System.out.println("Welcome" + name + regno);
+        System.out.println("Jenkins Build Successful! ");
+    }
+}
